2c8ab07e17fd349b046deaad4996d34ee702e674
DIALLO Abdourahamane <rahamanediallo108@gmail.com>
2025-04-10 01:01:34 -0400

    Utilisation de input et print

---
+++ introduction/e1.py
@@ -0,0 +1,8 @@
+# Introduction a l'utilsation des fonctions input et print
+
+# utilisation de la fonction print()
+
+# print("Ceci est notre premier programme en python.")
+
+nom = input("Quel est votre nom complet ?")
+print("Votre nom complet est : " + nom )
